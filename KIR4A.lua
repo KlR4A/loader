11a5addb4cd68ed477f2e32301de9e376bac9dbd
KlR4A <jajatmakam@gmail.com>
2026-09-25 00:21:09 +0700

    Create KIR4A.lua

---
+++ KIR4A.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "KlR4A", 
+	Text = "please wait", 
+	Icon = ""
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KlR4A/loader/refs/heads/main/KlR4A.lua"))()
